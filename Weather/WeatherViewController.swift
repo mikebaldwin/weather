96e92b748b991d056695834ccfaa8f65be3b5043
Mike Baldwin <mikembaldwin@gmail.com>
2018-07-09 10:43:33 -0700

    Rename ViewController to WeatherViewController

---
+++ Weather/WeatherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Michael Baldwin on 7/9/18.
+//  Copyright © 2018 mikebaldwin.co. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
