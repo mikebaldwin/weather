423e4c84c009beaf816bf5f6cbd830f7f98c4a53
Mike Baldwin <mikembaldwin@gmail.com>
2018-07-10 12:35:48 -0700

    Clean up DarkSky class

--- Weather/DarkSky.swift
+++ Weather/DarkSky.swift
@@ -9,24 +9,34 @@
 import Foundation
 
 class DarkSky {
+    private let session = URLSession.shared
+    private let baseURL = URL(string: "https://api.darksky.net/forecast/2c8e2d3d4cf1360a04149677b746cb17")!
     
     func downloadWeather(onSuccess passWeatherToCaller: @escaping (_ weather: Weather) -> Void) {
-        let session = URLSession.shared
-        let baseURL = URL(string: "https://api.darksky.net/forecast/")
-        let apiKey = "2c8e2d3d4cf1360a04149677b746cb17"
-        let coordinates = Location(latitude: 45.5122, longitude: 122.6587).coordinatesAsString
-        let finalURL = baseURL?.appendingPathComponent(apiKey).appendingPathComponent(coordinates)
-        let downloadTask = session.dataTask(with: finalURL!) { (data, response, error) in
+        let finalURL = assembleFinalURL()
+        let downloadTask = session.dataTask(with: finalURL) { (data, response, error) in
             if let data = data {
-                let decoder = JSONDecoder()
-                do {
-                    let weather = try decoder.decode(Weather.self, from: data)
+                if let weather = self.decodeWeatherData(data) {
                     passWeatherToCaller(weather)
-                } catch {
-                    print(error)
                 }
             }
         }
         downloadTask.resume()
+    }
+    
+    private func assembleFinalURL() -> URL {
+        let coordinates = Location(latitude: 45.5122, longitude: 122.6587).coordinatesAsString
+        return baseURL.appendingPathComponent(coordinates)
+    }
+    
+    private func decodeWeatherData(_ data: Data) -> Weather? {
+        let decoder = JSONDecoder()
+        do {
+            let weather = try decoder.decode(Weather.self, from: data)
+            return weather
+        } catch {
+            print(error)
+        }
+        return nil
     }
 }
