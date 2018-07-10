0271bf7be47f7fef3c3328aee5e54dd10ffa012b
Mike Baldwin <mikembaldwin@gmail.com>
2018-07-10 10:09:35 -0700

    Parse current conditions + temp from downloaded JSON

--- Weather/WeatherViewController.swift
+++ Weather/WeatherViewController.swift
@@ -12,29 +12,8 @@ class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        downloadWeatherData()
-    }
-
-    func downloadWeatherData() {
-        let session = URLSession.shared
-        let baseURL = URL(string: "https://api.darksky.net/forecast/")
-        let apiKey = "2c8e2d3d4cf1360a04149677b746cb17"
-        let coordinates = ["latitude" : "45.5122", "longitude" : "122.6587"]
-        let location = coordinates["latitude"]! + "," + coordinates["longitude"]!
-        let finalURL = baseURL?.appendingPathComponent(apiKey).appendingPathComponent(location)
-        
-        let downloadTask = session.dataTask(with: finalURL!) { (data, response, error) in
-            if let data = data {
-                print("data: ")
-                print(data)
-                print("\n")
-            }
-            if let response = response {
-                print("response:")
-                print(response)
-            }
-        }
-        downloadTask.resume()
+        let darkSky = DarkSky()
+        darkSky.downloadWeather()
     }
 
 }
