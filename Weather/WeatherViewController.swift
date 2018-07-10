50098b71a446cdfe4f8036a6c14c245d83d63541
Mike Baldwin <mikembaldwin@gmail.com>
2018-07-10 10:15:09 -0700

    Add labels to viewController

--- Weather/WeatherViewController.swift
+++ Weather/WeatherViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let darkSky = DarkSky()
