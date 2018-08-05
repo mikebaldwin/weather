dd8261a370ca309edff2ff7592474f1349b75320
Mike Baldwin <mikembaldwin@gmail.com>
2018-08-05 15:16:34 -0700

    Add weather icons

--- Weather/Models/Forecast.swift
+++ Weather/Models/Forecast.swift
@@ -9,8 +9,16 @@
 import Foundation
 
 class Forecast: Decodable {
+    // daily summary data
     var currentSummary: String
     var currentTemperature: String
+    var currentIcon: String
+    var currentChanceOfPrecipitation: String
+    var currentFeelsLikeTemperature: String
+    var currentHumidity: String
+    var currentUVIndex: String
+    
+    // Hourly data
     var todaysSummary: String
     var next12Hours: [Hour]
     
@@ -21,6 +29,12 @@ class Forecast: Decodable {
 
         currentSummary = rawServerResponse.currently.summary
         currentTemperature = String(Int(round(rawServerResponse.currently.temperature))) + "º"
+        currentIcon = rawServerResponse.currently.icon
+        currentChanceOfPrecipitation = String(Int(round(rawServerResponse.currently.chanceOfPrecipitation))) + "%"
+        currentFeelsLikeTemperature = String(Int(round(rawServerResponse.currently.feelsLikeTemperature))) + "º"
+        currentHumidity = String(rawServerResponse.currently.humidity) + "%"
+        currentUVIndex = String(rawServerResponse.currently.uvIndex)
+        
         todaysSummary = rawServerResponse.hourly.summary
         next12Hours = Array(hourlyData.dropLast(hourlyData.count - desiredHours))
     }
@@ -34,7 +48,22 @@ fileprivate struct RawServerResponse: Decodable {
     
     struct Currently: Decodable {
         let summary: String
+        let icon: String
+        let chanceOfPrecipitation: Double
         let temperature: Double
+        let feelsLikeTemperature: Double
+        let humidity: Double
+        let uvIndex: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case summary
+            case icon
+            case chanceOfPrecipitation = "precipProbability"
+            case temperature
+            case feelsLikeTemperature = "apparentTemperature"
+            case humidity
+            case uvIndex
+        }
     }
     
     struct Hourly: Decodable {
